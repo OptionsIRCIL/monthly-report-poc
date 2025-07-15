d558443742e47053ac68bc0646cceb8f7ea23474
Kyle Holter Vogel <44628526+kylehovo@users.noreply.github.com>
2025-07-15 16:28:59 -0500

    Add example SQL query (Listing Employees)

--- main.go
+++ main.go
@@ -9,12 +9,30 @@ import (
 func main() {
 
     // Connect to MySQL/MariaDB Database
-    db, err := sql.Open("mysql", "root:example@tcp(localhost:3306)/test")
+    db, err := sql.Open("mysql", "root:example@tcp(localhost:3306)/ILCWorkGroups")
     if err != nil {
         panic(err.Error())
     } else {
-        fmt.Println("Successful connection?")
+        fmt.Println("Successful connection")
     }
     defer db.Close()
+
+	// List employees
+    employees, err := db.Query("SELECT FirstName, LastName FROM DBA_t_Employees")
+    if err != nil {
+        panic(err.Error())
+    }
+    defer employees.Close()
+
+    fmt.Println("Employees:")
+    for employees.Next() {
+        var firstName sql.NullString
+        var lastName sql.NullString
+        err := employees.Scan(&firstName, &lastName)
+        if err != nil {
+            panic(err.Error())
+        }
+        fmt.Println(firstName.String, lastName.String)
+    }
 
 }
